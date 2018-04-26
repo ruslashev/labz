3b454a8fbb3f645d0574db5cbe437e1daaee9c5d
Ruslan Akbashev <ruslashev@gmail.com>
2018-04-26 12:06:48 +0300

    obf

--- 3koorse/andrey4.cc
+++ 3koorse/andrey4.cc
@@ -6,62 +6,94 @@
 
 using namespace std;
 
-int rand_in_range(int min, int max) {
-  return min + rand() % (max - min + 1);
+int rand_in_range(int min, int max)
+{
+    return min + rand() % (max - min + 1);
 }
 
-void print_set(const string &name, const set<int> &s) {
-  cout << name << " = { ";
-  for (int x : s)
-    cout << x << " ";
-  cout << "}" << endl;
-
+void print_set(const string &name, const set<int> &s)
+{
+    cout << name;
+    bool first = true;
+    for (int x : s)
+    {
+        if (first)
+        {
+            first = false;
+            cout << ": [";
+        } else
+        {
+            cout << ", ";
+        }
+        cout << x;
+    }
+    cout << "]" << endl;
 }
 
-bool contains_all_but_b(const set<int> &A, set<int> U, const set<int> &B) {
-  for (int x : B)
-    U.erase(x);
-  return U == A;
+bool contains_all_but_b(const set<int> &A, set<int> U, const set<int> &B)
+{
+    for (int x : B)
+    {
+        U.erase(x);
+    }
+    return U == A;
 }
 
-int main() {
-  srand(time(nullptr));
+int main()
+{
+    srand(time(nullptr));
 
-  int a = 0, b = 100, c = 50, d = 100, N = 10;
+    int a = 0, b = 100, c = 50, d = 100, N = 10;
 
-  set<int> U, A, B;
+    set<int> U, A, B;
 
-  while (U.size() != (size_t)N)
-    U.insert(rand_in_range(a, b));
+    while (U.size() != (size_t)N)
+    {
+        U.insert(rand_in_range(a, b));
+    }
 
-  for (int x = c; x <= d; ++x)
-    if (U.count(x))
-      B.insert(x);
+    for (int x = c; x <= d; ++x)
+    {
+        if (U.count(x))
+        {
+            B.insert(x);
+        }
+    }
 
-  while (!contains_all_but_b(A, U, B)) {
-    size_t idx = rand_in_range(0, U.size() - 1);
-    int elem = *next(U.cbegin(), idx);
-    if (c <= elem && elem <= d)
-      continue;
-    A.insert(elem);
-  }
+    while (!contains_all_but_b(A, U, B))
+    {
+        size_t idx = rand_in_range(0, U.size() - 1);
+        int elem = *next(U.cbegin(), idx);
+        if (c <= elem && elem <= d)
+        {
+            continue;
+        }
+        A.insert(elem);
+    }
 
-  set<int> AuB;
-  set_union(A.begin(), A.end(), B.begin(), B.end(), inserter(AuB, AuB.begin()));
+    set<int> un, na, nb, nanb, nnanb;
+    set_union(A.begin(), A.end(), B.begin(), B.end(), inserter(un, un.begin()));
 
-  set<int> nA, nB, nAnB, nnAnB;
-  set_difference(U.begin(), U.end(), A.begin(), A.end(), inserter(nA, nA.begin()));
-  set_difference(U.begin(), U.end(), B.begin(), B.end(), inserter(nB, nB.begin()));
-  set_intersection(nA.begin(), nA.end(), nB.begin(), nB.end(), inserter(nAnB, nAnB.begin()));
-  set_difference(U.begin(), U.end(), nAnB.begin(), nAnB.end(), inserter(nnAnB, nnAnB.begin()));
+    set_difference(U.begin(), U.end(), A.begin(), A.end(), inserter(na, na.begin()));
+    set_difference(U.begin(), U.end(), B.begin(), B.end(), inserter(nb, nb.begin()));
+    set_intersection(na.begin(), na.end(), nb.begin(), nb.end(), inserter(nanb, nanb.begin()));
+    set_difference(U.begin(), U.end(), nanb.begin(), nanb.end(), inserter(nnanb, nnanb.begin()));
 
-  print_set("U", U);
-  print_set("A", A);
-  print_set("B", B);
-  print_set("AuB", AuB);
-  print_set("nnAnB", nnAnB);
+    print_set("U", U);
+    print_set("A", A);
+    print_set("B", B);
+    print_set("un", un);
+    print_set("nnanb", nnanb);
 
-  copy_if(AuB.begin(), AuB.end(), ostream_iterator<int>(cout, " "),
-      [](int x) { return x % 2 != 0; });
+    cout << "equal? " << (un == nnanb) << endl;
+
+    for (int x : un)
+    {
+        if ((x % 2) != 0)
+        {
+            cout << x << " ";
+        }
+    }
+    cout << endl;
 }
 
